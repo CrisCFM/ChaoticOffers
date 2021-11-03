7762beef4a99b3dc78744247b87c0d3c852c4290
HaroldBarrera <83032032+HaroldBarrera@users.noreply.github.com>
2021-11-03 12:04:01 -0500

    Conexion con Node.js
    Conectar a la base de datos de WAMP mediante javascript.

---
+++ ProyectoDesarrolloWeb/app.js
@@ -0,0 +1,31 @@
+var mysql = require('mysql');
+
+//Crear la conexion
+var conexion = mysql.createConnection({
+    host:'localhost',
+    database:'parcial2',
+    user: 'root',
+    password: '123'
+});
+
+//Conectarse a la base de datos
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log("CONEXION A LA BASE DE DATOS EXITOSA")
+    }
+});
+
+//Hacer consultas
+conexion.query('SELECT * FROM usuarios', function(error, results, fields){
+    if(error){
+        throw error;
+    }else{
+        results.forEach(result => {
+            console.log(result);
+        })
+    }
+});
+
+conexion.end();
--- ProyectoDesarrolloWeb/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-// datos de configuracion de la conexión a la base de datos
-$host = "localhost"; //Dirección del servidor
-$usuario = "root"; //Nombre usuario de la base de datos
-$clave = "123"; // Contraseña de la base de datos
-$bd = "parcial2"; //Nombre de la base de datos
-?>
